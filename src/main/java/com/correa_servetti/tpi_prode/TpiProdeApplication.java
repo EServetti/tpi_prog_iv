18b6afb5452f8a8197c186c81a34aedb6c8e7ca8
unknown <eservetti2018@gmail.com>
2026-06-09 08:13:10 -0300

    First commit

---
+++ src/main/java/com/correa_servetti/tpi_prode/TpiProdeApplication.java
@@ -0,0 +1,13 @@
+package com.correa_servetti.tpi_prode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpiProdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpiProdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/correa_servetti/tpi_prode/TpiProdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correa_servetti.tpi_prode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpiProdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
