490d802cb59f29b6abeed50ae9ec208ee92dec96
unknown <eservetti2018@gmail.com>
2026-06-18 08:45:58 -0300

    Agregados arreglos al proyecto

--- src/main/java/com/correa_servetti/tpi_prode/models/Usuario.java
+++ src/main/java/com/correa_servetti/tpi_prode/models/Usuario.java
@@ -4,12 +4,17 @@ import com.correa_servetti.tpi_prode.models.enums.ROL;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter @Setter
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,15 +22,26 @@ public class Usuario {
     @Column(unique = true, nullable = false)
     String email;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     String password;
 
     @Column(nullable = false)
     String nombreUsuario;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     ROL rol;
 
     @Column
     LocalDate fechaRegistro;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + rol.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
